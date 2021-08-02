51f6f4db732a42ebf078b8702802481cbea65cbc
Jean Marcel <jeanvanzella91@gmail.com>
2021-08-02 16:10:10 -0300

    exercicios 5 e 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(), 'object', 'o retorno nao é um array.');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2, 'o array nao possui exatamente 2 itens');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(typeof productDetails()[0], 'object', 'o item retornado nao é um objeto');
+    assert.strictEqual(typeof productDetails()[1], 'object', 'o item retornado nao é um objeto');
+    // Teste que os dois objetos são diferentes entre si
+    assert.notStrictEqual(productDetails()[0], productDetails()[1], 'os parametros passados são iguais');
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true, 'final dos produtos nao termina em 123');
+    assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true, 'final dos produtos nao termina em 123');
   });
 });
